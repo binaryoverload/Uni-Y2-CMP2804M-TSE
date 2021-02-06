4e2ffca0ed93767b2fb436e88a326a0c3e323e1d
William Oldham <wegg7250@gmail.com>
2021-02-06 16:27:38 +0000

    Add a gitignore and some changes

--- code/AirplaneDataClass.py
+++ code/AirplaneDataClass.py
@@ -7,19 +7,28 @@ class AirplaneData:
         with open(filepath, 'r') as airplaneFile:
             next(airplaneFile)
             airplaneFileReader = csv.reader(airplaneFile, delimiter=',')
-            count = 0
+            validCount = 0
+            rowCount = 0
             for row in airplaneFileReader:
+                rowCount += 1
                 if (row[5][:2] == "EG"):
                     if (self.airports.count(row[5]) == 0):
                         self.airports.append(row[5])
                     date = datetime.utcfromtimestamp(int(row[2])).strftime("%Y-%m-%d")
                     key = (date, row[5])
-                    print (count)
-                    count += 1
+                    
+                    if validCount % 1000 == 0:
+                        print("Accepted data:", validCount, "|", "Row count:", rowCount)
+
+                    validCount += 1
+
                     if (list(self.__AirplaneData.keys()).count(key) == 1):
                         self.__AirplaneData[key] += 1
                     else:
                         self.__AirplaneData[key] = 1
+            print(validCount)
+
+                    
     def getData(self, date, location):
         try:
             return self.__AirplaneData[(date, location)]
@@ -29,6 +38,6 @@ class AirplaneData:
         return self.airports
 
 #TestCODE
-a = AirplaneData("C:/Users/benji/Downloads/output.csv")
+a = AirplaneData("output.csv")
 print(a.getAirports())
 print(a.getData("2020-04-24", "KMDT")) #Accepts date in YEAR-MONTH-DAY and 
