75c7121472be8e95f5845ece41727db1a1c62bd3
William Oldham <wegg7250@gmail.com>
2021-04-30 15:54:33 +0100

    Data thing

--- code/dataprocessing/SummariseCovid.py
+++ code/dataprocessing/SummariseCovid.py
@@ -8,11 +8,15 @@ with open("../../data/covid-data-jan-dec.csv", "r") as openFile:
         dateField = row.split(",")[0].split("/")
         date = datetime.date(int(dateField[2]), int(dateField[1]), int(dateField[0]))
         if date in covidDates:
-            covidDates[date] += int(row.split(",")[3])
+            covidDates[date]["daily"] += int(row.split(",")[3])
+            covidDates[date]["cumulative"] += int(row.split(",")[2])
         else:
-            covidDates[date] = int(row.split(",")[3])
+            covidDates[date] = {
+                "daily": int(row.split(",")[3]),
+                "cumulative": int(row.split(",")[2])
+            }
 
 with open("covid-data-summarised.csv", "w") as airportCounts:
-    airportCounts.write("date,count\n")
-    for date, count in sorted(covidDates.items(), key=lambda item: item[0]):
-        airportCounts.write(f"{date.strftime('%d/%m/%Y')},{count}\n")
+    airportCounts.write("date,daily,cumulative\n")
+    for date, data in sorted(covidDates.items(), key=lambda item: item[0]):
+        airportCounts.write(f"{date.strftime('%d/%m/%Y')},{data['cumulative']},{data['daily']}\n")
